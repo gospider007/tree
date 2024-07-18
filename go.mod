075b0858b3c01fc954e41879d8eaffc5a65f201c
gospider <2216403312@qq.com>
2024-07-18 09:53:28 +0800

    sync

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/gospider007/tree
 
-go 1.21.3
+go 1.22.5
 
 require github.com/gospider007/kinds v0.0.0-20231024093643-7a4424f2d30e
--- tree.go
+++ tree.go
@@ -1,21 +1,56 @@
 package tree
 
 import (
+	"slices"
 	"sync"
 
 	"github.com/gospider007/kinds"
 )
 
-type Client struct {
-	dataStr map[rune]*kinds.Set[string]
-	dataLen map[rune]*kinds.Set[int]
-	lock    sync.RWMutex
+type dataLenValue struct {
+	maxWord    bool
+	value      *kinds.Set[int]
+	isSort     bool
+	orderValue []int
 }
 
-func NewClient() *Client {
+func (obj *dataLenValue) Add(value int) {
+	obj.isSort = false
+	obj.value.Add(value)
+}
+func (obj *dataLenValue) Array() []int {
+	if obj.isSort {
+		return obj.orderValue
+	}
+	values := obj.value.Array()
+	slices.Sort(values)
+	if !obj.maxWord {
+		slices.Reverse(values)
+	}
+	obj.orderValue = values
+	obj.isSort = true
+	return values
+}
+
+type Client struct {
+	maxWord bool
+	dataStr map[rune]*kinds.Set[string]
+	dataLen map[rune]*dataLenValue
+	lock    sync.RWMutex
+}
+type ClientOption struct {
+	MaxWord bool
+}
+
+func NewClient(option ...ClientOption) *Client {
+	var opt ClientOption
+	if len(option) > 0 {
+		opt = option[0]
+	}
 	return &Client{
-		dataStr: map[rune]*kinds.Set[string]{},
-		dataLen: map[rune]*kinds.Set[int]{},
+		maxWord: opt.MaxWord,
+		dataStr: make(map[rune]*kinds.Set[string]),
+		dataLen: make(map[rune]*dataLenValue),
 	}
 }
 
@@ -29,7 +64,7 @@ func (obj *Client) Add(words string) {
 	word_len := len(wordrunes[1:])
 	if !obj.add(word_one, word_str, word_len) {
 		obj.lock.Lock()
-		obj.dataLen[word_one] = kinds.NewSet(word_len)
+		obj.dataLen[word_one] = &dataLenValue{value: kinds.NewSet(word_len), maxWord: obj.maxWord}
 		obj.dataStr[word_one] = kinds.NewSet(word_str)
 		obj.lock.Unlock()
 	}
@@ -54,13 +89,18 @@ func (obj *Client) Search(wordstr string) map[string]int {
 	defer obj.lock.RUnlock()
 	words := []rune(wordstr)
 	words_len := len(words)
-	for word_start, word := range words {
+	for word_start := 0; word_start < words_len; word_start++ {
+		word := words[word_start]
 		wordLens, ok := obj.dataLen[word]
 		if ok {
 			last_len := words_len - word_start - 1 //剩余长度=总长度减去-查询过的长度
-			for word_len := range wordLens.Map() {
+			for _, word_len := range wordLens.Array() {
 				if word_len > last_len {
-					continue
+					if obj.maxWord {
+						break
+					} else {
+						continue
+					}
 				}
 				if qg_str := string(words[word_start+1 : word_start+1+word_len]); obj.dataStr[word].Has(qg_str) {
 					searchVal := string(word) + qg_str
@@ -69,6 +109,8 @@ func (obj *Client) Search(wordstr string) map[string]int {
 					} else {
 						search_dic[searchVal] = 1
 					}
+					word_start += word_len
+					break
 				}
 			}
 		}
